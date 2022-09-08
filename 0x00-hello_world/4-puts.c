196121398c3ead678256ccdb76bfddb4ae5a4c24
charles <anayocharles18@gmail.com>
2022-09-08 06:41:16 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/*
-prints using the puts function
-*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
