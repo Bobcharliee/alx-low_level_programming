af2471095725483b2cb34a691db187e90451ae36
charles <anayocharles18@gmail.com>
2022-09-09 18:42:22 -0700

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	}
+	return (0);
+}
+
