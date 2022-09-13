fcbee0b11227c309089c32fbe8a8d290f43276b4
charles <anayocharles18@gmail.com>
2022-09-13 15:50:18 -0700

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program start
+ * Return: returns 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	
+	_putchar(str);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - printa a to z
+ */
+void print_alphabet(void)
+{
+	char letters = 'a';
+	
+	while (letters <= 'z')
+	{
+		_putchar(letters);
+		letters++;
+	}
+	_putchar('\n');
+}
