0eaacd8c71a95b74819a39b64b6d3ff1e9af629b
charles <anayocharles18@gmail.com>
2022-09-13 17:24:29 -0700

    loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - start
+ * @n - parameter
+ * Return: returns 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
