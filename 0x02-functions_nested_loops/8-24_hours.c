15ba5e3ccde2bac4881629d68e583ba450da2ee9
charles <anayocharles18@gmail.com>
2022-09-14 03:47:27 -0700

    task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * jack_bauer - start
+ */
+void jack_bauer(void)
+{
+	int min;
+	int sec;
+
+	for (min = 0; min < 24; min++)
+	{
+		for (sec = 0; sec < 60; sec++)
+		{
+			_putchar();
+		}
+	}
+}
