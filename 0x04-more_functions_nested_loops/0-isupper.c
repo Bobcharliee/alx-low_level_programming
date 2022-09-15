0b4ed686f889db5837d76ed64fd4616f033bd181
charles <anayocharles18@gmail.com>
2022-09-15 05:50:00 -0700

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - start
+ * @c: argument
+ * Return: return 0
+ */
+int _isupper(int c)
+{
+	char i;
+
+	for (i = 48; 1 < 57; i++)
+	{
+		if (c == i)
+			_putchar('1');
+	}
+	_putchar('0');
+	return (0);
+}
