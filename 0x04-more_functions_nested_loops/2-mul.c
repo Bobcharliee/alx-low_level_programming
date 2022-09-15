f52ffe9f2b97ee5db0025400875dd9de302ceee7
charles <anayocharles18@gmail.com>
2022-09-15 06:26:38 -0700

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - start
+ * @a - 1 argument
+ * @b - 2 argument
+ * Return: returns 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	_putchar(c + '0');
+	return (0);
+}
